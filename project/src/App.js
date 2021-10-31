92880d52c756c2b381eccfec021f4073096e2637
shikha-max <tshikha66@gmail.com>
2021-10-31 21:00:34 +0530

    created page1

--- project/src/App.js
+++ project/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import { Switch, Route } from 'react-router-dom'
-import Header from './components/Header'
+import Page1 from './components/Page1'
 import Home from './components/Home'
 function App() {
   return (
