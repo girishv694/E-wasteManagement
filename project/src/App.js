d838a4449dbca404a2b189b0abba732ec19290d7
pandeypushpendra3 <pandeypushpendra3@gmail.com>
2021-11-02 18:13:05 +0530

    page-11 some changes

--- project/src/App.js
+++ project/src/App.js
@@ -1,78 +1,67 @@
-import React from 'react'
-import './App.css'
-import { Switch, Route } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 // import Header from './components/Header'
 // import Home from './components/Home'
-import Page1 from './components/Page1'
-import Page7 from './components/Page7'
-import Page8 from './components/Page8'
-import { Page5 } from './components/Page5'
+import Page1 from "./components/Page1";
+import Page7 from "./components/Page7";
+import Page8 from "./components/Page8";
+import { Page5 } from "./components/Page5";
 
 // import Page1 from './components/Page1'
 // import Home from './components/Home'
-import Page2 from './components/Page2'
-<<<<<<< HEAD
-import Page3 from './components/Page3'
-import { Page11 } from './components/Page11'
-=======
-import Page3 from "./components/Page3"
-import Page10 from './components/Page10'
+import Page2 from "./components/Page2";
+import Page3 from "./components/Page3";
+import Page10 from "./components/Page10";
+import {Page11 }from "./components/Page11";
 
-
->>>>>>> 4380ee2c28e815cca37c78d14590c3a2005e2c11
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Switch>
-        <Route path='/page3'>
+        <Route path="/page3">
           <Page3 />
         </Route>
-<<<<<<< HEAD
-<Route path='/page11'>
-  <Page11/>
-  </Route>
-        <Route path='/scrap_sold'>
-=======
 
-        <Route path='/page7'>
->>>>>>> 4380ee2c28e815cca37c78d14590c3a2005e2c11
+        <Route path="/page7">
           <Page7></Page7>
         </Route>
 
-        <Route path='/page8'>
+        <Route path="/page8">
           <Page8></Page8>
         </Route>
 
-        <Route path='/page5'>
+        <Route path="/page11">
+          <Page11 />
+        </Route>
+
+        <Route path="/page5">
           <Page5 />
         </Route>
 
-        <Route path='/page2'>
+        <Route path="/page2">
           <Page2 />
         </Route>
 
-        <Route path='/signup'>
+        <Route path="/signup">
           <h1>Signup Page</h1>
         </Route>
 
-        <Route path='/checkout'>
+        <Route path="/checkout">
           <h1>Checkout Page</h1>
         </Route>
 
         <Route path="/page10">
-          
-          <Page10/>
+          <Page10 />
         </Route>
 
-    
-
-        <Route path='/'>
+        <Route path="/">
           <Page1 />
         </Route>
       </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
