445e310de4dc2a15ad58b6732236e1a4aac4894e
pandeypushpendra3 <pandeypushpendra3@gmail.com>
2021-11-02 20:59:01 +0530

    page-11 dialer added

--- project/src/components/Page11.jsx
+++ project/src/components/Page11.jsx
@@ -19,7 +19,25 @@ function Page11() {
               <input type="text" palceholder="+91-Mobile Number"></input>
           </div>
          <button><p>Continue</p></button>
-          <div></div>
+          <div className="grid-container">
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>-</div>
+              
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>++</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div>++</div>
+              <div>,</div>
+              <div>0</div>
+              <div>.</div>
+              <div>--</div>
+          </div>
         </Div>
       </Link>
     </div>
