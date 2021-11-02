393e55e71114cf1dc0543c8e45a38f7f73c78246
Sagar Kurewar <sagarkurewar@gmail.com>
2021-11-02 11:07:09 +0530

    page8 font style added

--- project/src/components/Page1.jsx
+++ project/src/components/Page1.jsx
@@ -2,30 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../css/Page.css'
 import { Div } from './Container'
-<<<<<<< HEAD
-<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Crimson+Text" />
-function Page1() {
-    return (
-        <div className="header">
-            <Link to="/login">
-                <Div>
-                    <img src="Images/page1Image.png" alt="home"></img>
-
-
-                    <div id="heading">
-                        <p> SCAVENGER</p>
-                    </div>
-
-                    <div id="para">
-                        <p>With more scraps comes  more responsibility</p>
-                    </div>
-
-                </Div>
-
-            </Link>
-        </div>
-    )
-=======
 // PLEASE PUT FONT LINKS ON INDEX.HTML
 function Page1() {
   return (
@@ -47,7 +23,6 @@ function Page1() {
       </Link>
     </div>
   )
->>>>>>> 181893406d76bfca2e2d95ea7d80c5ad006892d5
 }
 
 export default Page1
--- project/src/components/Page2.jsx
+++ project/src/components/Page2.jsx
@@ -1,22 +1,6 @@
 import { Div } from './Container'
 import '../css/page2.css'
-<<<<<<< HEAD
-import { Button } from './Button'
 import { Link } from 'react-router-dom'
-const Page2 = () => {
-    return <>
-        <Div>
-            <p id="language">Select app language</p>
-
-            <Link to="/page7">
-                <Button>Hindi</Button>
-            </Link>
-
-        </Div>
-
-=======
-import { Link } from 'react-router-dom'
->>>>>>> 181893406d76bfca2e2d95ea7d80c5ad006892d5
 
 // import { Button } from './Button'
 const Page2 = () => {
