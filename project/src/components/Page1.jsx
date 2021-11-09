cf380759f9ee126ca83db7de4132112e63fd5ba6
Sagar Kurewar <sagarkurewar@gmail.com>
2021-11-09 19:06:37 +0530

    page1 done

--- project/src/components/Page1.jsx
+++ project/src/components/Page1.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../css/page1.css'
-import { Div } from './Container'
+import { Container } from './Container_sk'
 function Page1() {
     return (
-        <div className="header">
-            <Link to="/page2">
-                <Div>
-               <div  id="container">
-               <div>
-             <div>
-             <img src="Images/page1Image.png" alt="" ></img>
 
-             </div>
+        <Link to="/page2">
+            <Container>
+                <div id="container">
+                    <div>
+                        <div>
+                            <img src="Images/page1Image.png" alt="" ></img>
 
-                    <div id="heading">
-                        <p>SCAVENGER</p>
+                        </div>
+
+                        <div id="heading">
+                            <p>SCAVENGER</p>
+                        </div>
+
+                        <div id="para">
+                            <p> With more scraps comes  more responsibility</p>
+                        </div>
+
                     </div>
+                </div>
+            </Container>
+        </Link>
 
-                    <div id="para">
-                       <p> With more scraps comes  more responsibility</p>
-                    </div>
-
-                    </div>
-               </div>
-                    </Div>
-            </Link>
-        </div>
     )
 }
 
