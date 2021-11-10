cd6748efa3799b367ec5e10ae00028fe97dc467e
Sagar Kurewar <sagarkurewar@gmail.com>
2021-11-10 18:54:58 +0530

    payment page done

--- project/src/components/Payment.jsx
+++ project/src/components/Payment.jsx
@@ -1,8 +1,53 @@
+import { Container } from "./Container_sk"
+import "../css/payment.css"
 const Payment = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <Container>
+
+            <div id="b_arrow_div">
+                <img id="b_arrow" src="Images/back_arrow.png" alt="" />
+            </div>
+
+
+            <div id="paradiv">
+                Select money acceptance mode
+            </div>
+
+            <div id="div_upi" className="method_display">
+                <div>
+                    <img className="upi_img" src="Images/upi_logo.png" alt="" />
+                </div>
+                <div className="payment_para">
+                    Get money in the bank account through UPI
+                </div>
+            </div>
+
+            <div id="div_gpay" className="method_display">
+                <div>
+                    <img className="gpay_img" src="Images/gpay_logo.png" alt="" />
+                </div>
+                <div className="payment_para">
+                    Get money in the Gpay wallet
+                </div>
+            </div>
+
+            <div id="div_cash" className="method_display">
+                <div>
+                    <img className="cash_img" src="Images/cash_logo.png" alt="" />
+                </div>
+                <div className="payment_para">
+                    Get money in cash by our pick up executive
+                </div>
+            </div>
+
+
+
+            <button id="payment_btn">
+                Submit
+            </button>
+        </Container>
+
+
     )
 }
 
