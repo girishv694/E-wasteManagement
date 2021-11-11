f5efcab08e8db33e9317217ca697c283aa39ea4a
Sagar Kurewar <sagarkurewar@gmail.com>
2021-11-11 19:07:49 +0530

    reciept upload done

--- project/src/components/Page11.jsx
+++ project/src/components/Page11.jsx
@@ -1,60 +1,60 @@
-import {React} from 'react'
+import { React } from 'react'
 import { Link } from 'react-router-dom'
-import"../css/Page.css"
+import "../css/Page.css"
 import '../css/page11.css'
 import { Div } from './Container'
 import { useState } from 'react'
 // Page-3 Pushpendra
 function Page11() {
-   const [show,setShow] = useState(false)
+  const [show, setShow] = useState(false)
   return (
-   
-     
-        <Div>
-        <div >
+
+
+    <Div>
+      <div >
         <img src="http://localhost:3000/Images/backarrow1.svg" alt="" id="arrow" />
 
-          <div className='page11-login'>
-            <p clasName='login'>Please Login</p>
-            <p className='page11-phone'>Please enter your phone number</p>
-          </div>
-          <div className='page11-input'>
-            <input onFocus={()=>{
-              setShow(true)
-            }}
-              type='text'
-              placeholder='     +91-Mobile Number'
-              className='input_p11'
-            ></input>
-          </div>
-          <Link to='/ewaste'>
+        <div className='page11-login'>
+          <p clasName='login'>Please Login</p>
+          <p className='page11-phone'>Please enter your phone number</p>
+        </div>
+        <div className='page11-input'>
+          <input onFocus={() => {
+            setShow(true)
+          }}
+            type='text'
+            placeholder='     +91-Mobile Number'
+            className='input_p11'
+          ></input>
+        </div>
+        <Link to='/ewaste'>
           <button className='but_p11'>
             <p>Continue</p>
           </button>
-          </Link>
-          <div className='grid-container' id={show? "disp":""}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div id="blue">-</div>
+        </Link>
+        <div className='grid-container' id={show ? "disp" : ""}>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div id="blue">-</div>
 
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div id="blue">+</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
-            <div id="blue">*</div>
-            <div>,</div>
-            <div>0</div>
-            <div>/</div>
-            <div id="blue"></div>
-          </div>
-      
-    </div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div id="blue">+</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+          <div id="blue">*</div>
+          <div>,</div>
+          <div>0</div>
+          <div>/</div>
+          <div id="blue"></div>
+        </div>
+
+      </div>
     </Div>
-    
+
   )
 }
 
