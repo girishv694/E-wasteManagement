cfd62e1489b6bfa014b27ec53cfed5fa32848e7e
Sagar Kurewar <sagarkurewar@gmail.com>
2021-11-11 15:05:36 +0530

    page93 done

--- project/src/components/Page93.jsx
+++ project/src/components/Page93.jsx
@@ -1,62 +1,72 @@
 import { Container } from "./Container_sk"
 import "../css/page93.css"
 
-
 const Page93 = () => {
     return (
         <Container>
-            <div id="e-waste">
-                <p>E-Waste</p>
+            <div id="b_arrow_93">
+                <img id="b_arrow_93img" src="Images/back_arrow.png" alt="" />
             </div>
 
-            <div id="content_div">
-                <div>
-                    <img className="prod_img" src="Images/mbl_frame.png" alt="" />
-                    <p>Mobile</p>
-                </div>
-               
-                <div>
-                    <img className="prod_img" src="Images/ac_frame.png" alt="" />
-                    <p>AC</p>
-                </div>
-               
-                <div>
-                    <img className="prod_img" src="Images/tv_frame.png" alt="" />
-                    <p>CRT</p>
-                </div>
-               
-                <div>
-                    <img className="prod_img" src="Images/lcd.png" alt="" />
-                    <p>LCD/LED</p>
-                </div>
-               
-                <div>
-                    <img className="prod_img" src="Images/battery.png" alt="" />
-                    <p>Battery</p>
-                </div>
-               
-                <div>
-                    <img className="prod_img" src="Images/fridge.png" alt="" />
-                    <p>Fridge</p>
-                </div>
-               
-                <div>
-                    <img className="prod_img" src="Images/w_machine.png" alt="" />
-                    <p>Washing</p>
-                </div>
-               
-                <div>
-                    <img className="prod_img" src="Images/music.png" alt="" />
-                    <p>Music</p>
-                </div>
-               
-                <div>
-                    <img className="prod_img" src="Images/printer.png" alt="" />
-                    <p>Printer</p>
-                </div>
-               
+            <div id="ewaste">
+                E-Waste
             </div>
-       </Container>
+
+            <div className="waste_category" id="top_margin">
+                <div className="waste_item">
+                    <img src="Images/mbl_frame.png" alt="" className="waste_item_img" />
+                    Mobile
+                </div>
+
+                <div className="waste_item">
+                    <img src="Images/ac_frame.png" alt="" className="waste_item_img" />
+                    AC
+                </div>
+
+                <div className="waste_item">
+                    <img src="Images/tv_frame.png" alt="" className="waste_item_img" />
+                    CRT
+                </div>
+
+            </div>
+
+            <div className="waste_category">
+                <div className="waste_item">
+                    <img src="Images/lcd.png" alt="" className="waste_item_img" />
+                    LCD/LED
+                </div>
+
+                <div className="waste_item">
+                    <img src="Images/printer.png" alt="" className="waste_item_img" />
+                    Printer
+                </div>
+
+                <div className="waste_item">
+                    <img src="Images/battery.png" alt="" className="waste_item_img" />
+                    Battery
+                </div>
+
+            </div>
+
+            <div className="waste_category">
+                <div className="waste_item">
+                    <img src="Images/fridge.png" alt="" className="waste_item_img" />
+                    Fridge
+                </div>
+
+                <div className="waste_item">
+                    <img src="Images/w_machine.png" alt="" className="waste_item_img" />
+                    Washing Machine
+                </div>
+
+                <div className="waste_item">
+                    <img src="Images/music.png" alt="" className="waste_item_img" />
+                    Music System
+                </div>
+
+            </div>
+
+        </Container>
     )
 }
 
