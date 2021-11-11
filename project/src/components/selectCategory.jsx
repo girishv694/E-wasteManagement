386947c8037d258f703bf6a0fa7465375c28f6f6
Shivang <iamshivang@outlook.com>
2021-11-11 18:24:49 +0530

    shiv

--- project/src/components/selectCategory.jsx
+++ project/src/components/selectCategory.jsx
@@ -6,30 +6,16 @@ import { Link } from 'react-router-dom'
 import '../css/category.css'
 export const Category = () => {
   const [show, setShow] = useState(false)
-  const [file, setFile] = useState([])
-  console.log(file)
   const uploadImg = () => {
-    setShow(!show)
+    setShow(true)
   }
 
   function onFileChange(event) {
-    // console.log(event);
-    setFile(event.target.files[0])
-  }
-  function onFileUpload() {
-    // Create an object of formData
     const formData = new FormData()
-
-    // Update the formData object
-    formData.append('image_urls', file)
-
-    // Details of the uploaded file
-    console.log(file)
-
-    // Request made to the backend api
-    // Send formData object
+    formData.append('image_urls', event.target.files[0])
     axios.post('http://localhost:3001/products/img', formData)
   }
+
   return (
     <>
       <Div>
@@ -53,33 +39,25 @@ export const Category = () => {
           </div>
         </div>
 
+        {/* <img src='Images/upload.svg' alt='' /> Upload scrap item's image */}
         {show ? (
           <div className='imgupload'>
-            <div >
-              {/* <img src='Images/upload.svg' alt='' /> Upload scrap item's image */}
-              <input
-                type='file'
-                encType='multipart/form-data'
-                id='fileupload'
-                name='fileupload'
-                onChange={(e) => onFileChange(e)}
-              />
-              <button
-                id='upload-btn'
-                onClick={() => {
-                  onFileUpload()
-                }}
-              >
-                Upload scrap item's image
-              </button>
-            </div>
+            <input
+              class='custom-file-input'
+              type='file'
+              encType='multipart/form-data'
+              id='fileupload'
+              name='fileupload'
+              onChange={(e) => onFileChange(e)}
+            />
           </div>
         ) : null}
 
-       <Link to="/bargain">
-       <div className='select' >
-          <div> Not satisfied with the price</div>
-        </div></Link>
+        <Link to='/bargain'>
+          <div className='select'>
+            <div> Not satisfied with the price</div>
+          </div>
+        </Link>
       </Div>
     </>
   )
